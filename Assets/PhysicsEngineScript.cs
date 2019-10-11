dd6546d4ec66504cb305b7f23547a8355e25d4c3
Ceirios <480227@chrome.escg.ac.uk>
2019-10-11 11:52:11 +0100

    Need to do more reaserch

--- Assets/PhysicsEngineScript.cs
+++ Assets/PhysicsEngineScript.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class PhysicsEngineScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
+        double Mass;
+        Mass = 1;
         
     }
-
     // Update is called once per frame
     void Update()
     {
