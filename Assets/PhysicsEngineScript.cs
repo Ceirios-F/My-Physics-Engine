ce3f2f1d56b4d508f17942a849ce65bf3e33bac7
Ceirios <480227@chrome.escg.ac.uk>
2019-10-18 14:40:34 +0100

    Do more reasherch

--- Assets/PhysicsEngineScript.cs
+++ Assets/PhysicsEngineScript.cs
@@ -11,13 +11,59 @@ public class PhysicsEngineScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        double Mass;
-        Mass = 1;
-        
+
     }
     // Update is called once per frame
     void Update()
     {
+        Vector3 sdfg = new Vector3(5, 15, 3);
+        var dsm = Vector3.Scale(new Vector3(), new Vector3(5, 4, 2));
+
         
+
+
+        int CurrentShotTypes = 1;
+
+        switch (CurrentShotTypes)
+        {
+            case 1:
+                {
+                   
+                    Debug.Log("make red power ranger");
+                    break;
+                }
+            case 2:
+                {
+                    Debug.Log("make blue power ranger");
+                    break;
+                }
+            case 3:
+                {
+                    Debug.Log("make green power ranger");
+                    break;
+                }
+            case 4:
+                {
+                    Debug.Log("make green power ranger");
+                    break;
+                }
+
+            case 5:
+                {
+                    Debug.Log("make green power ranger");
+                    break;
+                }
+
+            default:
+                break;
+        }
+
     }
+
+    
+}
+
+public class AirSpace
+{
+   
 }
