68db0c567faff37456df06ddece7b0b3afae7d3c
Ceirios <480227@chrome.escg.ac.uk>
2019-10-24 16:32:59 +0100

    Following totorial

--- Assets/PhysicsEngineScript.cs
+++ Assets/PhysicsEngineScript.cs
@@ -7,28 +7,33 @@ using UnityEngine;
 
 public class PhysicsEngineScript : MonoBehaviour
 {
+    public Vector3 Acceleration;
+    public Vector3 Velocity;
+    public Vector3 ForceAccum;
+    public Vector3 Gravity;
+    public Rigidbody rb;
+    public float Mass;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
-        Vector3 sdfg = new Vector3(5, 15, 3);
-        var dsm = Vector3.Scale(new Vector3(), new Vector3(5, 4, 2));
-
-        
-
-
+        Gravity = new Vector3(0, -1, 0);
         int CurrentShotTypes = 1;
 
         switch (CurrentShotTypes)
         {
             case 1:
                 {
-                   
+                    Mass = 2.0f;
+                    Velocity = new Vector3 (0.0f, 0.0f, 5.0f);
+                    Acceleration = new Vector3(0.0f, -1.0f, 0.0f);
+                    rb.AddForce(Gravity * Mass);
+                    transform.position += Velocity * Time.deltaTime + (Acceleration * Time.deltaTime * 0.5f);
                     Debug.Log("make red power ranger");
                     break;
                 }
