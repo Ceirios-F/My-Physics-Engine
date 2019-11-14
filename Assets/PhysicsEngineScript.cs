04309a00a09aeebc126fb6a417881f87312ab43a
Ceirios <480227@chrome.escg.ac.uk>
2019-11-14 16:28:05 +0000

    Finish at home

--- Assets/PhysicsEngineScript.cs
+++ Assets/PhysicsEngineScript.cs
@@ -12,8 +12,9 @@ public class PhysicsEngineScript : MonoBehaviour
     public Vector3 ForceAccum;
     public Vector3 Gravity;
     public Rigidbody rb;
-    public float Mass;
-    float Duration;
+    public float InveseMass;
+    public float damping;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,27 +24,26 @@ public class PhysicsEngineScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Duration > 0.0 )
-            {
 
-
-
-
-            }
-        
-        Gravity = new Vector3(0, -1, 0);
-     
-        Mass = 2.0f;
-        Velocity = new Vector3 (0.0f, 0.0f, 5.0f);
-        Acceleration = new Vector3(0.0f, -1.0f, 0.0f);
-        rb.AddForce(Gravity * Mass);
+        InveseMass = 0.0f;
+        Gravity = new Vector3(0, -1, 0);            
+        Velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+        Acceleration = new Vector3(0.0f, 0.0f, 0.0f);
+        rb.AddForce(Gravity * InveseMass);
         transform.position += Velocity * Time.deltaTime + (Acceleration * Time.deltaTime * 0.5f);
-        Debug.Log("make red power ranger");
-
+        //Debug.Log("make red power ranger");
         Vector3 resultingAcc = Acceleration;
-        resultingAcc.Scale(ForceAccum);
-        rb.AddForce(ConstantForce);
-        
+        resultingAcc.Scale (ForceAccum * InveseMass);
+        Velocity.Scale (resultingAcc * Time.deltaTime);
+        Velocity *= (damping * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            InveseMass = 2.0f;
+            Velocity = new Vector3(0.0f, 0.0f, 35.0f);
+            Acceleration = new Vector3(-1.0f, 0.0f, 0.0f);
+            damping = 0.99f;
+        }
     }
 
     
@@ -56,7 +56,7 @@ public class Partical
     public Vector3 ForceAccum;
     public Vector3 Gravity;
     public Rigidbody rb;
-    public float Mass;
+    public float InveseMass;
   
 
 
